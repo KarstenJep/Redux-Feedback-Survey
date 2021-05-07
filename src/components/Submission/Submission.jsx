ee904c04e227d86d3110d8bc005c864bbd3e6eda
Karsten Jepsen <Kjepsen86@gmail.com>
2021-05-06 23:27:55 -0500

    Added submission display

--- src/components/Submission/Submission.jsx
+++ src/components/Submission/Submission.jsx
@@ -2,14 +2,19 @@ function Submission() {
 
     const handleNext = () => {
         alert('Survey resetting... one moment please.')
-        // TODO - Reset survey data
         history.push('/');
     }
 
     return (
+        <>
+            <div>
+                <p><h2>Feedback!</h2></p>
+            </div>
              <div>
+                <p><h2>Thank You!</h2></p>
                 <button className="next" onClick={handleNext}>Leave New Feedback</button>
             </div>
+        </>
     )
 }
 
