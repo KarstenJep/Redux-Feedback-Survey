c357014e3f51467e0644e063b390374d8c006a08
Karsten Jepsen <Kjepsen86@gmail.com>
2021-05-06 22:33:38 -0500

    Setup submission handler

--- src/components/Submission/Submission.jsx
+++ src/components/Submission/Submission.jsx
@@ -1,4 +1,11 @@
 function Submission() {
+
+    const handleNext = () => {
+        alert('Survey resetting... one moment please.')
+        // TODO - Reset survey data
+        history.push('/');
+    }
+
     return (
              <div>
                 <button className="next" onClick={handleNext}>Leave New Feedback</button>
